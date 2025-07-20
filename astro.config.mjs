751ec449744a4326ca5e8192ab6264a85b57ab57
gidragir <ilyabalter1@gmail.com>
2025-07-20 12:33:15 +0500

    update dep, add purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import purgecss from "astro-purgecss";
+
 export default defineConfig({
   site: "https://gidragir.github.io",
   base: "resume_1c",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [purgecss()],
 });
